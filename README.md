1a61b290645815e64245e7f5f3e05a61735b421a
alexbuntaran <alex.buntaran@gmail.com>
2024-02-05 16:49:50 -0800

    added readme updates

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CSC466-FinalProject
+Authors: Aditya Manikonda, Alex Buntaran, Joanna Chou
+
+## Setup
+```
+pip install pandas
+pip install -U scikit-learn
+```
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.pipeline import make_pipeline
+
 def main():
     print("predict values :)")
 
